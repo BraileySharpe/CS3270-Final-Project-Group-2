31925a6881efa9c7f71ae30a11133ea9434bc2f8
cdanie23 <cdanie23@my.westga.edu>
2026-04-20 10:41:55 -0400

    Adds a CNN model with mock data to generate chess board evaluations

--- evaluation_model/model_training.py
+++ evaluation_model/model_training.py
@@ -1,10 +1,13 @@
 import torch
-from mock_data import generate_data
+from evaluation_model.mock_data import generate_data
 import torch.nn as nn
 import torch.optim as optim
 from torch.optim.lr_scheduler import ReduceLROnPlateau
 from torch.utils.data import DataLoader, random_split, Dataset
 from torch.utils.data import Dataset
+from Controller.Controller import generate_training_data
+from sklearn.model_selection import train_test_split
+
 
 class MyDataset(Dataset):
 	def __init__(self, data):
@@ -14,8 +17,9 @@ class MyDataset(Dataset):
 		return len(self.data)
 
 	def __getitem__(self, idx):
-		tensor, evaluation = self.data[idx]
-
+		tensor = self.data.iloc[idx, 0]
+		evaluation = self.data.iloc[idx, 1]
+		
 		tensor = torch.tensor(tensor, dtype=torch.float32)
 		evaluation = torch.tensor(evaluation, dtype=torch.float32)
 
@@ -23,14 +27,19 @@ class MyDataset(Dataset):
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-mock_dataset = generate_data()
+dataframe = generate_training_data()
 
-dataset = MyDataset(mock_dataset)
+train_df, test_df = train_test_split(dataframe, test_size=0.3, random_state=56)
 
-train_size = int(0.7 * len(dataset))
-test_size = len(dataset) - train_size
+mean = train_df["Evaluation"].mean()
+std= train_df["Evaluation"].std()
 
-train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
+train_df["Evaluation"] = (train_df["Evaluation"] - mean) / std
+test_df["Evaluation"] = (test_df["Evaluation"] - mean) / std
+
+train_dataset = MyDataset(train_df)
+test_dataset = MyDataset(test_df)
+
 
 train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
 test_loader = DataLoader(test_dataset, batch_size=16, shuffle=False)
@@ -45,14 +54,17 @@ class CNN_Model(nn.Module):
 			nn.ReLU(), 
 			nn.Conv2d(64, 128, kernel_size=3, padding=1),
 			nn.ReLU(),
-			nn.AdaptiveAvgPool2d(1),
 		)
-		self.fc1= nn.Linear(128, 1)
+		self.fc= nn.Sequential(
+			nn.Flatten(), 
+			nn.Linear(128*8*8, 256),
+			nn.ReLU(), 
+			nn.Linear(256,1)
+		)
 
 	def forward(self,x):
 		x = self.feature_extract(x)
-		x = torch.flatten(x, 1)
-		x = self.fc1(x)
+		x = self.fc(x)
 		return x
 
 model = CNN_Model().to(device)
@@ -65,7 +77,7 @@ criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5)
 
-num_epochs = 10
+num_epochs = 40
 
 
 for epoch in range(num_epochs):
@@ -75,18 +87,23 @@ for epoch in range(num_epochs):
 	total = 0
 
 	for tensor, evaluation in train_loader:
+
 		tensor, evaluation = tensor.to(device), evaluation.to(device)
 
 		optimizer.zero_grad()
 		output = model(tensor)
+		
 		evaluation = evaluation.unsqueeze(1)
+		
 		loss = criterion(output, evaluation)
 		loss.backward()
 		optimizer.step()
 		
 		train_loss += loss.item()
-
+	
+	train_loss /= len(train_loader)
 	train_losses.append(train_loss)
+	
 	model.eval()
 	test_loss = 0 
 
@@ -98,7 +115,7 @@ for epoch in range(num_epochs):
 			loss = criterion(output, evaluation)
 			test_loss += loss.item()
 
-
+	test_loss /= len(test_loader)
 	test_losses.append(test_loss)
 
 	scheduler.step(test_loss)
@@ -113,5 +130,7 @@ print(f"Average Training Loss: {average_train_loss:.4f}")
 average_test_loss = sum(test_losses) / len(test_losses)
 print(f"Average Testing Loss: {average_test_loss:.4f}")
 
+torch.save(model.state_dict(), "evaluation_cnn_model.pth")
+print("Model Saved")
 
 
